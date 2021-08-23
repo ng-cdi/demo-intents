9ac329e565464e6d20805216d898d945db93e6d4
crotsos <c.rotsos@lancaster.ac.uk>
2021-08-23 15:57:34 +0100

    Adding boilerplate juju template

---
+++ mano/vnfd/layers/ciscoProxy/reactive/ciscoProxy.py
@@ -0,0 +1,45 @@
+from charmhelpers.core.hookenv import (
+    action_get,
+    action_fail,
+    action_set,
+    status_set,
+)
+from charms.reactive import (
+    clear_flag,
+    set_flag,
+    when,
+    when_not,
+)
+import charms.sshproxy
+
+@when('sshproxy.configured')
+@when_not('ciscoProxy.installed')
+def install_ciscoProxy():
+    # Do your setup here.
+    #
+    # If your charm has other dependencies before it can install,
+    # add those as @when() clauses above., or as additional @when()
+    # decorated handlers below
+    #
+    # See the following for information about reactive charms:
+    #
+    #  * https://jujucharms.com/docs/devel/developer-getting-started
+    #  * https://github.com/juju-solutions/layer-basic#overview
+    #
+    set_flag('ciscoProxy.installed')
+    status_set('active', 'Ready!')
+
+@when('actions.touch')
+def touch():
+    """Touch a file."""
+    err = ''
+    try:
+        filename = action_get('filename')
+        cmd = ['touch {}'.format(filename)]
+        result, err = charms.sshproxy._run(cmd)
+    except:
+        action_fail('command failed: {}'.format(err))
+    else:
+        action_set({'output': result})
+    finally:
+        clear_flag('actions.touch')
