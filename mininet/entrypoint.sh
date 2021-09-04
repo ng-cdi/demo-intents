37cd9cff6e2b66dc95ac249686180371186d93c1
crotsos <c.rotsos@lancaster.ac.uk>
2021-09-04 06:50:43 +0100

    Update action on router upgrade charms
    Signed-off-by: crotsos <c.rotsos@lancaster.ac.uk>

--- mininet/entrypoint.sh
+++ mininet/entrypoint.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+echo
+echo "Starting Dynamips"
+echo
+
+echo "dynamips --idle-pc 0x641a83e4 -P 7200 -T 2001 -A 3001 \
+	-p 1:PA-4E -s 1:0:tap:tap0 -s 1:1:tap:tap1 -s 1:2:tap:tap3 \
+	-p 2:PA-GE -s 2:0:tap:tap2 \
+	-C $DYNAGEN_ROUTER_CFG -X $DYNAGEN_IMG &"
+
+dynamips --idle-pc 0x641a83e4 -P 7200 -T 2001 -A 3001 \
+	-p 1:PA-4E -s 1:0:tap:tap0 -s 1:1:tap:tap1 -s 1:2:tap:tap10 \
+	-p 2:PA-GE -s 2:0:tap:tap2 \
+	-C $DYNAGEN_ROUTER_CFG -X $DYNAGEN_IMG &
+
+
 ## hack this in, otherwise we start before onos is up fully and die
 echo "MININET waiting for ONOS"
 
@@ -15,8 +30,7 @@ echo "ONOS is up! waiting 5 more seconds to be sure"
 
 sleep 5
 
-## run these just to make sure?
-
+# run these just to make sure?
 curl -X POST --user onos:rocks http://localhost:8181/onos/v1/applications/org.onosproject.openflow/active
 curl -X DELETE --user onos:rocks http://localhost:8181/onos/v1/applications/org.onosproject.fwd/active
 curl -X POST --user onos:rocks http://localhost:8181/onos/v1/applications/org.onosproject.imr/active
